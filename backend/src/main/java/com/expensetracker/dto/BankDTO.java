fb00a8d694e010a3ffaf05fd2e2d1e2238ba03c3
Yash <seehra.a@yahoo.com>
2025-07-12 14:26:01 +0200

    Changes to the DTO

--- backend/src/main/java/com/expensetracker/dto/BankDTO.java
+++ backend/src/main/java/com/expensetracker/dto/BankDTO.java
@@ -1,16 +1,3 @@
 package com.expensetracker.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class BankDTO {
-    private String id;
-    private String name;
-    private String logo;
-}
+public record BankDTO(String id, String name, String logo) {}
