4b91087fcf45a33c9b7a851bf7c4722a7784bb7a
Yash <seehra.a@yahoo.com>
2025-07-18 21:41:26 +0200

    Added auth context and updated the routes to be protected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import LoginPage from "./pages/LoginPage.tsx";
 import {useTheme} from "./hooks/UseTheme.tsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import ProtectedRoute from "./components/ProtectedRoutes.tsx";
 
 function App() {
     const { theme } = useTheme();
 
   return (
       <div className={`app ${theme}`}>
-        <LoginPage />
+          <Routes>
+              <Route path="/" element={
+                  <ProtectedRoute>
+                  <HomePage />
+                  </ProtectedRoute>
+              } />
+              <Route path="/login" element={
+                      <LoginPage />
+              } />
+          </Routes>
       </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from "./Provider/ThemeProvider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import {AuthProvider} from "./Provider/AuthProvider.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ThemeProvider>
-    <App />
-      </ThemeProvider>
-  </StrictMode>,
+      <BrowserRouter>
+          <AuthProvider>
+              <ThemeProvider>
+                  <App />
+              </ThemeProvider>
+          </AuthProvider>
+      </BrowserRouter>
+  </StrictMode>
 )
